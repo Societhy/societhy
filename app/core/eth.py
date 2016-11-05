0bf8105722a319e9fa70a3181a4ba929f9c8dc6a
Simon Vadée <simon.vadee@gmail.com>
2016-11-05 15:09:36 +0100

    add ethereum-related controller

---
+++ app/core/eth.py
@@ -0,0 +1,41 @@
+import time
+import ethjsonrpc
+from base64 import b64decode, b64encode
+
+from flask import session, request, Response
+from models import users
+
+def gen_linked_key(user):
+	print(user)
+	return {
+		"data": "OK",
+		"status": 200
+	}
+
+def key_was_generated(user, address):
+	print(user, address)
+	return {
+		"data": "OK",
+		"status": 200
+	}
+
+def import_new_key(user, key):
+	print(key)
+	return {
+		"data": "OK",
+		"status": 200
+	}
+
+ def export_and_delete_key(user, key):
+	print(key)
+	return {
+		"data": "OK",
+		"status": 200
+	}
+
+ def export_key(user, key):
+	print(key)
+	return {
+		"data": "OK",
+		"status": 200
+	}
