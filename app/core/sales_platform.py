e7e4a8493d2f405b726edcc4909217f231f40905
Cedric Voinnet <cedric.voinnet@epitech.eu>
2017-02-09 11:22:52 +0100

    + methodes model product

--- app/core/sales_platform.py
+++ app/core/sales_platform.py
@@ -1,4 +1,5 @@
 from bson.objectid import ObjectId
+from bson.json_util import dumps
 from models.product import products, ProductsDocument
 from models.organization import organizations
 
@@ -8,7 +9,11 @@ def addProduct(newProduct):
         return {'data': 'Organization does not exist', 'status': 400}
     product = ProductsDocument(newProduct)
     product.save()
-    return { 'data': product, 'status': 200}
+    return {'data': product, 'status': 200}
+
+def getProductsByOwner(ownerId):
+    productsFound = products.find({'owner': ObjectId(ownerId)})
+    return {'data': dumps(productsFound), 'status': 200}
 
 def updateProduct(productId, updateFields):
     result = products.update_one({'_id': ObjectId(productId)}, updateFields)
@@ -21,3 +26,9 @@ def removeProduct(productId):
     if result.deleted_count < 1:
         return {'data': 'Product does not exist', 'status': 400}
     return {'data': 'Product deleted', 'status': 200}
+
+def changeProductStock(productId, value):
+    result = update_one({'_id': ObjectId(productId)}, {'$inc': {'stock': value}})
+    if result.modified_count:
+        return {'data': 'Product does not exist', 'status': 400}
+    return {'data': result.raw_result, 'status': 200}
