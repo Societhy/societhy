7dfcf929950f639d0f88b576dab79d8fe6b2695b
tuorg <roman.grout@epitech.eu>
2017-03-09 14:58:56 +0100

    core fini

--- app/core/utils.py
+++ app/core/utils.py
@@ -1,30 +1,63 @@
+"""
+This modules contains tools function that might me used by the models or the controllers.
+"""
 from copy import copy
 from bson.objectid import ObjectId
 import json
 
 class UserJSONEncoder(json.JSONEncoder):
+	"""
+	Used to change ObjectId object in sring in order to be transmitted on a HTTP pipe.
+	Otherwise, ObjectId can't be transmitted.
+	"""
     def default(self, o):
         if isinstance(o, ObjectId):
             return str(o)
         return json.JSONEncoder.default(self, o)
 
 def fromWei(value):
+	"""
+	value : the value to compute.
+
+	This function is used to divide the value with 10^18.
+	User to get Wei from Ether (unit conversion)
+	"""
 	return value / (10 ** 18)
 
 def toWei(value):
+	"""
+	value : the value to compute.
+
+	This function is used to multiply the value with 10^18.
+	User to get Ether from Wei (unit conversion)
+	"""
 	return int(value * (10 ** 18))
 
 def serializeUser(user):
+	"""
+	Legacy function.
+	Dedicated to be deleted in the next refactoring.
+	"""
 	serialized = copy(user)
 	serialized.update({"_id": ObjectId(user.get('_id'))})
 	return serialized
 
 def deserializeUser(user):
+	"""
+	Legacy function.
+	Dedicated to be deleted in the next refactoring.
+	"""
+
 	deserialized = copy(user)
 	deserialized.update({"_id": str(user.get('_id'))})
 	return deserialized
 
 def normalizeAddress(address, hexa=False):
+	"""
+	address : the address.
+
+	Used to re-syntax an Ethereum address.
+	"""
 	if len(address) > 40:
 		address = address[len(address) - 40:]
 
