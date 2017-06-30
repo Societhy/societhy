560824133490a639165975ef5426ec936960742f
Roman Grout <roman.grout@epitech.eu>
2017-06-30 17:57:35 +0200

    contact preference OK

--- app/web/static/assets/js/controllers/UserPreferenceCtrl.js
+++ app/web/static/assets/js/controllers/UserPreferenceCtrl.js
@@ -1,4 +1,21 @@
 app.controller('userPreferenceCtrl', function($scope, $http, $timeout, $rootScope, $state, $controller, $sessionStorage) {
     var ctrl = this;
 
+    this.notification_preference = $rootScope.user.notification_preference;
+
+    this.update = function ()
+    {
+        console.log("hello");
+        $http.post('/updateUser',
+            {
+                "notification_preference" : this.notification_preference
+            }
+        ).then(function(response) {
+            $rootScope.user = response.data;
+            ctrl.notification_preference = $rootScope.user.notification_preference;
+        }, function(error)
+        {
+            console.log(error);
+        });
+    }
 });
